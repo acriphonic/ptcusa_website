e91f71d7694391796e27dd504aed215040d3f8e4
acriphonic <acriphonic@gmail.com>
2013-12-27 14:21:54 +0000

    basic website

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PtcusaWebsite::Application.config.secret_key_base = '0d862166a849a1f138242491e6b306ecb8bfec9d976f4916ec4de57edc6fb083466519a68e4ec4f0aa8d49f9ed62babbd06d397048d6cdfa8b750a9e03e217fe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PtcusaWebsite::Application.config.session_store :cookie_store, key: '_ptcusa_website_session'
